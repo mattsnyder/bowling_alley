47949a33cf4cdb61e8f93b76b9a13a75aa8bc653
Matt Snyder <matt@cranialpulse.com>
2016-10-13 17:25:02 -0700

    Calculate bowler score based on recorded rolls

--- lib/game.ex
+++ lib/game.ex
@@ -4,31 +4,30 @@ defmodule BowlingAlley.Game do
   @name __MODULE__
   
   def start_link() do
-    IO.puts "#{__MODULE__}#start_link"
-
     GenServer.start_link(__MODULE__, :ok, name: @name)
   end
 
   def add_bowler(bowler_name) do
-    IO.puts "#{__MODULE__}#add_player"
     GenServer.call(@name, {:add_bowler, bowler_name})
   end
 
   def add_roll(bowler_name, pins) do
-    IO.puts "#{__MODULE__}#add_roll"
     GenServer.call(@name, {:add_roll, bowler_name, pins})
   end
+
+  def bowler_score(bowler_name) do
+    GenServer.call(@name, {:bowler_score, bowler_name})
+  end    
   
   def bowlers do
     GenServer.call(@name, {:bowlers})
   end
   
   def init(:ok) do
-#    IO.puts "#{__MODULE__}#init"
-#    Process.send_after self(), :started, 1
     {:ok, %{}} # Initial state
   end
 
+  
   # def handle_info(:started, []) do
   #   IO.puts "#{__MODULE__}#started"
   #   {:noreply, []}
@@ -47,6 +46,13 @@ defmodule BowlingAlley.Game do
     rolls = GenServer.call(pid, {:add_roll, pins})
     
     {:reply, rolls, state}
+  end
+
+  def handle_call({:bowler_score, bowler_name}, _from, state) do
+    {:ok, pid} = Map.fetch(state, bowler_name)
+    score = GenServer.call(pid, {:score})
+    
+    {:reply, score, state}
   end
 
   def handle_call({:bowlers}, _from, state) do
--- lib/scorer.ex
+++ lib/scorer.ex
@@ -12,8 +12,39 @@ defmodule BowlingAlley.Scorer do
     {:ok, []}
   end
 
-  def handle_call({add_roll, pins}, _from, state) do
+  def handle_call({:add_roll, pins}, _from, state) do
+    IO.inspect(state ++ [pins])
     {:reply, state ++ [pins], state ++ [pins]}
   end
+
+  def handle_call({:score}, _from, state) do
+    {:reply, score(state), state}
+  end
+
+  def score(rolls) do
+    Enum.map(rolls, &fill_in_strikes(&1))
+    |> List.flatten
+    |> fill_empty_frames
+    |> Enum.chunk(2) # Divide into frames
+    |> Enum.chunk(3,1) # Group frames with potentially related frames
+    |> Enum.map(&frame_score(&1))
+    |> Enum.sum
+  end
+
+  defp frame_score([[10, :strike], next1, next2]), do: score_strike(Enum.filter([10] ++ next1 ++ next2, &is_integer(&1)))
+  defp frame_score([[_,:tbd], _, _]), do: 0
+  defp frame_score([[roll1, roll2], _, _]) when roll1 + roll2 < 10, do: roll1 + roll2
+  defp frame_score([[roll1, roll2], bonus_frame, _]) when roll1 + roll2 == 10, do: frame_score([[:spare],bonus_frame])
+  defp frame_score([[:spare], [bonus_roll, _]]) when is_integer(bonus_roll), do: 10 + bonus_roll
+  defp frame_score([[:spare], [:tbd, _]]), do: 0
+
+  defp score_strike(rolls) when length(rolls) < 3, do: 0
+  defp score_strike(rolls), do: Enum.slice(rolls, 0..3) |> Enum.sum
+
+  defp fill_in_strikes(10), do: [10, :strike]
+  defp fill_in_strikes(x), do: x
+
+  defp fill_empty_frames(rolls) when length(rolls) < 22, do: fill_empty_frames(rolls ++ [:tbd]) # Roll to be determined
+  defp fill_empty_frames(rolls), do: rolls
   
 end
